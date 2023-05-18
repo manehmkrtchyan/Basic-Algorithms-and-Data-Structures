898081e362f7a75aa67cba73be2123efbe248946
manehmkrtchyan <107489584+manehmkrtchyan@users.noreply.github.com>
2023-05-18 18:36:35 +0400

    Add files via upload

---
+++ singly_linked_list.py
@@ -0,0 +1,78 @@
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+
+    def __repr__(self) -> str:
+        return str((self.value, f"next id: {id(self.next)}"))
+    
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+
+    def push_front(self, value):
+        new_head = Node(value)
+        if len(self) != 0:
+            self.head, new_head.next = new_head, self.head
+        else: self.head = new_head
+
+    def insert(self, value, pos):
+        current = self.head
+        for i in range(1, pos):
+            current = current.next
+        tmp = current.next
+        current.next = (new := Node(value))   
+        new.next = tmp
+
+    def push_back(self, value):
+        node = Node(value)      
+        if self.head:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = node
+        else:
+            self.head = node
+
+    def find(self, value):
+        for idx, val in enumerate(self):
+            if val.value == value:
+                return idx 
+
+    def delete(self, pos):
+        current = self.head 
+        for i in range(1, pos):
+            current = current.next 
+        new_next = current.next.next
+        to_be_deleted = current.next
+        del to_be_deleted
+        current.next = new_next
+
+    def __len__(self):
+        count = 0
+        node = self.head
+        while node:
+            count += 1
+            node = node.next
+        return count
+    
+    def __iter__(self):
+        current = self.head
+        while current:
+            yield current
+            current = current.next
+    
+    def __repr__(self) -> str:
+        return str([node.value for node in self])
+
+ls = LinkedList()
+ls.push_front(1)
+ls.push_front(2)
+ls.push_front(3)
+ls.push_front(4)
+ls.insert(7, 2)
+ls.push_back(9)
+ls.push_back(8)
+ls.push_back(10)
+print(ls)
+print(ls.find(34))
